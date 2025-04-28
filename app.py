62c6a48ebad468480fe420159b9d60bb4d8a33b7
cigrirepo <adamcigri@gmail.com>
2025-04-27 23:24:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ st.set_page_config(page_title="Senzu Financial Insights", layout="wide")
 logo = Image.open("senzu_logo.png")
 col1, col2, col3 = st.columns([1, 8, 1])
 with col2:
-    st.image(logo, width=300)
+    st.image(logo, width=200)
 
 
 # ── Keys / Secrets ─────────────────────────────────────────────────
